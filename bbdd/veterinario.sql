06bca33ff7fb63da453c44bec60a605ea16db6d3
Antonio-Olivares4 <olivares.lopez.antonio-vladimir@iescamas.es>
2024-10-11 09:14:34 +0200

    He hecho el script de veterianario.

--- bbdd/veterinario.sql
+++ bbdd/veterinario.sql
@@ -6,18 +6,12 @@ CREATE TABLE animales (
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
     peso DECIMAL(10, 2) NOT NULL,
-    raza text, 
+    raza text,
     tipo text
 );
 
 INSERT INTO animales (nombre, descripcion, peso, raza, tipo) VALUES
-('Jabón de Lavanda', 'Jabón artesanal de lavanda', 5.99, 50),
-('Jabón de Rosas', 'Jabón suavizante de rosas', 6.49, 40),
-('Jabón de Menta', 'Jabón refrescante de menta', 5.89, 60),
-('Jabón de Avena', 'Jabón exfoliante de avena', 5.49, 35),
-('Jabón de Cítricos', 'Jabón energizante de cítricos', 6.99, 45),
-('Jabón de Caléndula', 'Jabón calmante de caléndula', 6.79, 25),
-('Jabón de Chocolate', 'Jabón nutritivo de chocolate', 7.49, 30),
-('Jabón de Café', 'Jabón rejuvenecedor de café', 7.89, 20),
-('Jabón de Almendras', 'Jabón hidratante de almendras', 6.19, 40),
-('Jabón de Karité', 'Jabón suavizante de manteca de karité', 8.49, 15);
+('Chispita', 'Un perro bodeguero muy grande.', 8, 'bodeguero', 'Perro'),
+('Lucero', 'Gato negro muy arisco.', 10, 'callejero', 'Gato'),
+('Piolín', 'Canario amarillo', 0.4, 'canario', 'Ave'),
+('Elena', 'La gallina favorita del granjero James', 2, 'gallina_blanca', 'Ave');
